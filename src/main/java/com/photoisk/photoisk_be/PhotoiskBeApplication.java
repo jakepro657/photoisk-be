1475cc671820f04eabce28b71fd3fbbebad79a74
jakepro657 <jakepro657@gmail.com>
2024-12-24 17:06:56 +0900

    first commit

---
+++ src/main/java/com/photoisk/photoisk_be/PhotoiskBeApplication.java
@@ -0,0 +1,13 @@
+package com.photoisk.photoisk_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotoiskBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhotoiskBeApplication.class, args);
+	}
+
+}
